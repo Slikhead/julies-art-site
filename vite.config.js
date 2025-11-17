f4d7cab0b57cf4d9531a2bdf47a9f8f83b299827
Slikhead <pete.geo63@gmail.com>
2025-11-17 19:29:11 +1300

    Fix Vite config (remove incorrect custom rollupOptions)

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // 🟦 Tell Vite to use src/index.jsx as the entry point
-  build: {
-    rollupOptions: {
-      input: "src/index.jsx",
-    },
-  },
 });
-
